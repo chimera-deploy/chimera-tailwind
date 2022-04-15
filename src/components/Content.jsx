80472db66a0036c83262629ad3b9ba55e050ec37
Will <wor101@gmail.com>
2022-04-15 10:15:57 -0400

    implemented sidebar links

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -8,9 +8,8 @@ const Content = () => {
     <>
       {sectionHeaders.map(header => {
         const paragraphs = caseStudy[header].split(/\n/)
-        //console.log(paragraphs.split(/\n/))
         return (
-          <div key={header} className='md:mx-64 mx-12'>
+          <div key={header} id={header} className='md:mx-64 mx-12'>
             <h2 className='font-koulen text-tyrianpurple text-4xl text-center my-6'>{header}</h2>
             <div className='font-sans text-lg space-y-3'>
               {paragraphs.map((paragraph, idx) => <p key={`${paragraph.length}-${header}-${idx}`}>{paragraph}</p>)}
@@ -19,23 +18,7 @@ const Content = () => {
         )
       })}
     </>
-
-  )
-  
+  )  
 }
 
 export default Content
-
-
-
-/*
-
-    <div className='h-screen md:mx-64 mx-12'>
-      <h2 className='font-koulen text-tyrianpurple text-4xl text-center my-6'>Deployments in Modern Systems</h2>
-      <div className='font-sans text-lg space-y-3'>
-        <p>Incididunt ut duis nisi proident dolor voluptate ut ex. Cillum reprehenderit sit aute ad duis est consectetur. Sunt sint fugiat magna nisi do. Laboris id nisi ad veniam nulla proident proident mollit consequat. Excepteur aute reprehenderit velit irure consectetur nulla aliquip. Qui ex id aute aute anim ullamco eiusmod. Nisi reprehenderit fugiat aute commodo nulla ut commodo ut velit.</p>
-        <p>Cillum enim ipsum eiusmod esse voluptate minim ipsum eu culpa fugiat esse laborum nulla. Elit ex eu enim magna amet deserunt minim ea. Deserunt ex tempor est amet ea est exercitation velit. Deserunt do consequat excepteur esse dolor nulla et voluptate magna esse exercitation duis labore. Pariatur minim adipisicing sint nisi esse dolor nulla veniam elit amet velit magna. Voluptate adipisicing occaecat nisi occaecat proident. Dolor Lorem exercitation tempor dolor aute ut adipisicing mollit ea dolor cillum in.</p>
-        <p>Exercitation ad culpa cillum eu ut id cupidatat mollit dolor. Elit ex dolore tempor do amet sit dolore Lorem duis nostrud. Ex nulla deserunt mollit ex culpa quis cupidatat sint veniam sint. Excepteur qui nisi culpa minim minim eu consequat.</p>
-      </div>
-    </div>
-*/
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const Sidebar = ({ sidebarVisible }) => {
+
   return (
     <div className={sidebarVisible ? 'md:block hidden fixed top-24 left-0' : 'hidden'}>
       <ul className='font-san text-tyrianpurple text-xl px-4 space-y-2'>
-        <li><Link to='/#deployments' className='hover:text-2xl hover:font-bold'>Deployments</Link></li>
-        <li><Link to='/#canary' className='hover:text-2xl hover:font-bold'>Canary Deployments</Link></li>
-        <li><Link to='/#solutions' className='hover:text-2xl hover:font-bold'>Current Solutions</Link></li>        
-        <li><Link to='/#features' className='hover:text-2xl hover:font-bold'>Central Features</Link></li>
-        <li><Link to='/#using' className='hover:text-2xl hover:font-bold'>Using Chimera</Link></li>
-        <li><Link to='/#design' className='hover:text-2xl hover:font-bold'>Design Decisions</Link></li>
-        <li><Link to='/#future' className='hover:text-2xl hover:font-bold'>Future Work</Link></li>
+        <li><a href='#Deployments%20In%20Modern%20Systems' className='hover:text-2xl hover:font-bold'>Deployments</a></li>
+        <li><a href='#Components%20of%20a%20Canary%20Deployment' className='hover:text-2xl hover:font-bold'>Canary Deployments</a></li>
+        <li><a href='#Current%20Solutions' className='hover:text-2xl hover:font-bold'>Current Solutions</a></li>        
+        <li><a href='#Central%20Features%20of%20Chimera' className='hover:text-2xl hover:font-bold'>Central Features</a></li>
+        <li><a href="#Using%20Chimera:%20The%20Dev's%20Perspective" className='hover:text-2xl hover:font-bold'>Using Chimera</a></li>
+        <li><a href="#Technical%20Challenges%20&%20Design%20Decisions" className='hover:text-2xl hover:font-bold'>Design Decisions</a></li>
+        <li><a href='#Future%20Work' className='hover:text-2xl hover:font-bold'>Future Work</a></li>      
       </ul>
     </div>
   )
 }
 
 export default Sidebar
+
+/*
+
+*/
