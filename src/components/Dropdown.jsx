35c404b71118864ba95848858bf23d7930e15a0a
Will <wor101@gmail.com>
2022-04-15 11:18:34 -0400

    added boring github icon

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import octoCat from '../images/Octocat/Octocat.png'
+import tyrianKitty from '../images/github-tyrianpurple-kitty.png'
 
 const Dropdown = ({toggle, dropdownVisible}) => {
   return (
@@ -8,7 +9,7 @@ const Dropdown = ({toggle, dropdownVisible}) => {
       <Link to="/" className="p-4 hover:bg-flame hover:text-white rounded-lg whitespace-nowrap ">Home</Link>
       <Link to="/casestudy" className="p-4 hover:bg-flame hover:text-white rounded-lg whitespace-nowrap">Case Study</Link>
       <Link to="/team" className="p-4 hover:bg-flame hover:text-white rounded-lg whitespace-nowrap">Our Team</Link>
-      <a href="https://github.com/chimera-deploy/Chimera" className="h-20"><img src={octoCat} className="h-20"></img></a>
+      <a href="https://github.com/chimera-deploy/Chimera" className="h-20"><img src={tyrianKitty} className="h-14"></img></a>
     </nav>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,6 +4,23 @@ const Sidebar = ({ sidebarVisible }) => {
 
   return (
     <div className={sidebarVisible ? 'md:block hidden fixed top-24 left-0' : 'hidden'}>
+      <ul className='font-koulen text-tyrianpurple text-xl px-4 space-y-2' >
+        <li><a href='#Deployments%20In%20Modern%20Systems' className='hover:text-2xl hover:text-bold'>Deployments</a></li>
+        <li><a href='#Components%20of%20a%20Canary%20Deployment' className='hover:text-2xl hover:text-bold '>Canary Deployments</a></li>
+        <li><a href='#Current%20Solutions' className='hover:text-2xl hover:text-bold '>Current Solutions</a></li>        
+        <li><a href='#Central%20Features%20of%20Chimera' className='hover:text-2xl hover:text-bold '>Central Features</a></li>
+        <li><a href="#Using%20Chimera:%20The%20Dev's%20Perspective" className='hover:text-2xl hover:text-bold '>Using Chimera</a></li>
+        <li><a href="#Technical%20Challenges%20&%20Design%20Decisions" className='hover:text-2xl hover:text-bold '>Design Decisions</a></li>
+        <li><a href='#Future%20Work' className='hover:text-2xl hover:text-bold '>Future Work</a></li>      
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
+
+/*
+    <div className={sidebarVisible ? 'md:block hidden fixed top-24 left-0' : 'hidden'}>
       <ul className='font-san text-tyrianpurple text-xl px-4 space-y-2'>
         <li><a href='#Deployments%20In%20Modern%20Systems' className='hover:text-2xl hover:font-bold'>Deployments</a></li>
         <li><a href='#Components%20of%20a%20Canary%20Deployment' className='hover:text-2xl hover:font-bold'>Canary Deployments</a></li>
@@ -14,11 +31,19 @@ const Sidebar = ({ sidebarVisible }) => {
         <li><a href='#Future%20Work' className='hover:text-2xl hover:font-bold'>Future Work</a></li>      
       </ul>
     </div>
-  )
-}
-
-export default Sidebar
+*/
 
 /*
+    <div className={sidebarVisible ? 'md:block hidden fixed top-24 left-0' : 'hidden'}>
+      <ul className='font-koulen text-tyrianpurple text-xl px-4 space-y-2' >
+        <li><a href='#Deployments%20In%20Modern%20Systems' className='px-2 rounded-lg hover:text-2xl hover:text-white hover:bg-flame hover:text-bold '>Deployments</a></li>
+        <li><a href='#Components%20of%20a%20Canary%20Deployment' className='px-2 rounded-lg hover:text-2xl hover:text-white hover:bg-flame hover:text-bold '>Canary Deployments</a></li>
+        <li><a href='#Current%20Solutions' className='px-2 rounded-lg hover:text-2xl hover:text-white hover:bg-flame hover:text-bold '>Current Solutions</a></li>        
+        <li><a href='#Central%20Features%20of%20Chimera' className='px-2 rounded-lg hover:text-2xl hover:text-white hover:bg-flame hover:text-bold '>Central Features</a></li>
+        <li><a href="#Using%20Chimera:%20The%20Dev's%20Perspective" className='px-2 rounded-lg hover:text-2xl hover:text-white hover:bg-flame hover:text-bold '>Using Chimera</a></li>
+        <li><a href="#Technical%20Challenges%20&%20Design%20Decisions" className='px-2 rounded-lg hover:text-2xl hover:text-white hover:bg-flame hover:text-bold '>Design Decisions</a></li>
+        <li><a href='#Future%20Work' className='px-2 rounded-lg hover:text-2xl hover:text-white hover:bg-flame hover:text-bold '>Future Work</a></li>      
+      </ul>
+    </div>
 
 */
